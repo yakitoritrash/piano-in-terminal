7679ad86484cedd531e7b26f0146e1635fb0f3c8
Arch me <kishudwivedi30@gmail.com>
2026-02-06 22:35:59 +0530

    more funcs

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from pick_choice import pick_choice
+
 def main():
     choice_list = ["improv", "songs"]
     choice = pick_choice(choice_list);
-    print(choice)
+    if choice == "improv":
+        pass;
+    elif choice == "songs":
+        pass;
 
 main()
