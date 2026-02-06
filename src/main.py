8de8ac2aa7f83ee3fcc60c42b9f46b7235f3654c
Arch me <kishudwivedi30@gmail.com>
2026-02-06 20:55:47 +0530

    Moved venv to correct folder

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from pick_choice import pick_choice
+def main():
+    choice_list = ["improv", "songs"]
+    pick_choice(choice_list);
+
+main()
---
+++ src/pick_choice.py
@@ -0,0 +1,31 @@
+import sys, tty, termios
+
+def pick_choice(options):
+    print("Pick a choice:")
+    old_settings = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin.fileno())
+    current_index = 0;
+    try:
+        while (True):
+            for option in options:
+                if current_index == options.index(option):
+                    print(f"> {option}");
+                else:
+                    print(f"  {option}");
+            key = sys.stdin.read(1)
+            if key == "j":
+                if current_index == 0:
+                    current_index += 1
+                else:
+                    current_index -= 1;
+            elif key == "k":
+                if current_index == 0:
+                    current_index += 1;
+                else:
+                    current_index -= 1;
+            elif key == "\n":
+                break;
+            sys.stdout.write(f"\033[{len(options)}A")
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+        print()
