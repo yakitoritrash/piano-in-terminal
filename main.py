e3e1f446770303c410f473463474bd59cb20f5ee
Arch me <kishudwivedi30@gmail.com>
2026-02-06 19:18:20 +0530

    src

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def main():
-    print("hello")
+import inquirer
 
+def main():
+    options = ['improv', 'songs']
+    questions = [inquirer.List('choice', message='Pick a choice', choices = options)]
+    answers = inquirer.prompt(questions) # {'choice': 'improv'}
+    print(answers)
 main();
